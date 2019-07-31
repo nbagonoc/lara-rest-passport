3f87a0626e7fd9eaf23176108298f7b9b2295c08
NiccuBagonoc <niccu.bagonoc@jpj.com>
2019-07-31 14:51:45 +0800

    Removing '/' from route

--- routes/api.php
+++ routes/api.php
@@ -20,9 +20,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 /////////////////////////////////////////
 // Register
 /////////////////////////////////////////
-Route::post('/register', 'Api\AuthController@register');
+Route::post('register', 'Api\AuthController@register');
 
 /////////////////////////////////////////
 // Login
 /////////////////////////////////////////
-Route::post('/login', 'Api\AuthController@login');
+Route::post('login', 'Api\AuthController@login');
